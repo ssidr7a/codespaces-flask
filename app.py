b69a403d8e0bda524287fe5a081d377be01b67b0
ssidr7a <160396882+ssidr7a@users.noreply.github.com>
2024-03-03 11:26:16 +0000

    Помощь бота

--- app.py
+++ app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import pandas as pd
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Gendalf")
+questions_df = pd.read_excel('questions.xlsx')
+questions_list = questions_df['Question'].tolist()
+
+@app.route('/')
+def index():
+    random_question = random.choice(questions_list)
+    questions_list.remove(random_question) 
+    return render_template('index.html', question=random_question)
+
+@app.route('/new_question', methods=['POST'])
+def new_question():
+    if len(questions_list) == 0:
+        return "Вопросов больше не осталось."
+    random_question = random.choice(questions_list)
+    questions_list.remove(random_question)
+    return render_template('index.html', question=random_question)
+
+if __name__ == '__main__':
+    app.run(debug=True)
